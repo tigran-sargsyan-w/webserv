5c1c8ca2e9864a76d9469b130c0ecf0d3b0069ba
dsemenov <dsemenov@student.42lyon.fr>
2026-03-17 20:25:15 +0100

    feat: create and init socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
 #include "WebServ.hpp"
 
 int	main()
@@ -7,5 +11,28 @@ int	main()
 
 	WebServ serv;
 
+	int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverFd == -1)
+	{
+		std::cerr << "socket() failed\n";
+		return (1);
+	}
+
+	std::cout << "Server socked created, FD = " << serverFd << "\n";
+
+
+	sockaddr_in addr;
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(8080);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(serverFd, (sockaddr *) &addr, sizeof(addr)))
+	{
+		std::cerr << "Error binding socket\n";
+		return (1);
+	}
+
+	close(serverFd);
 	return (0);
 }

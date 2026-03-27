6dabcf0d85dd70e3e38fda262dcbfb8635a08acc
Extraden <53974460+Extraden@users.noreply.github.com>
2026-03-27 04:08:36 +0100

    feat: add response with 404 code

---
+++ include/Response.hpp
@@ -0,0 +1,14 @@
+#ifndef RESPONSE_HPP
+# define REQUEST_HPP
+
+class Response
+{
+    public:
+        Response();
+        ~Response();
+
+    private:
+        // Add response-related members here (e.g., status code, headers, body)
+};
+
+#endif

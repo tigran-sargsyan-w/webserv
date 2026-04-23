e089c011d128f580c3334f6dd5203ffd1338307f
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-04-23 22:55:48 +0200

    feat: update WebServ class to include server configuration in setup method

--- include/WebServ.hpp
+++ include/WebServ.hpp
@@ -10,20 +10,21 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include "Config.hpp"
 
 class WebServ
 {
+	private:
+		int _serverSocket;
+
 	public:
 		WebServ();
 		WebServ(const WebServ& other);
 		~WebServ();
 		WebServ& operator=(const WebServ& other);
 
-		int setup();
+		int setup(const ServerConfig &serverConfig);
 		int run();
-	
-	private:
-		int _serverSocket;
 };
 
 #endif

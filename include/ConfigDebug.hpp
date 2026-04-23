244dee65bbf54c6e4645f23d4a64399c50966eab
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-04-23 21:40:14 +0200

    feat: add ConfigDebug header for color-coded debug output

---
+++ include/ConfigDebug.hpp
@@ -0,0 +1,12 @@
+#ifndef CONFIGDEBUG_HPP
+#define CONFIGDEBUG_HPP
+
+namespace ConfigDebug
+{
+	static const char *const reset = "\033[0m";
+	static const char *const lexer = "\033[36m";
+	static const char *const parser = "\033[35m";
+	static const char *const validator = "\033[33m";
+}
+
+#endif

2150b1f343a48d8328cac033f2324b1cf3d98926
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-04-23 22:56:33 +0200

    feat: enhance main function to support dynamic configuration file loading and error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #include "WebServ.hpp"
+#include "ConfigParser.hpp"
 
-int	main()
+#include <exception>
+#include <iostream>
+
+int	main(int argc, char **argv)
 {
-	WebServ serv;
+	std::string configPath = "configs/default.conf";
+	if (argc > 1)
+		configPath = argv[1];
 
-	serv.setup();
-	serv.run();
-	return (0);
+	Config config;
+	try
+	{
+		config = ConfigParser::parseFile(configPath);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << "\n";
+		return (1);
+	}
+	if (config.servers.empty())
+	{
+		std::cerr << "No server blocks found in config\n";
+		return (1);
+	}
+
+	WebServ serv;
+	if (serv.setup(config.servers[0]) != 0)
+		return (1);
+	return (serv.run());
 }

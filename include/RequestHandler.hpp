f1d54a4bdee770826f8403acc85cbeca9e852257
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-03 22:40:49 +0200

    feat: update handleRequest to include remoteAddr parameter in RequestHandler

--- include/RequestHandler.hpp
+++ include/RequestHandler.hpp
@@ -11,7 +11,7 @@ public:
     RequestHandler();
     ~RequestHandler();
 
-    static Response handleRequest(const Request &request, const RouteConfig &route, const ServerConfig &server);
+    static Response handleRequest(const Request &request, const RouteConfig &route, const ServerConfig &server, const std::string &remoteAddr);
     static Response handleStatic(const Request &request);
 };
 

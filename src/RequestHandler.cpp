3791bed8c105d4f1e6179d8f91076c0028c8cf8c
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-03 20:17:32 +0200

    feat: enhance RequestHandler with improved CGI context handling and header management

--- src/RequestHandler.cpp
+++ src/RequestHandler.cpp
@@ -11,6 +11,7 @@
 #include <utils.hpp>
 #include "Config.hpp"
 #include <vector>
+#include <map>
 
 RequestHandler::RequestHandler() {}
 
@@ -18,25 +19,25 @@ RequestHandler::~RequestHandler() {}
 
 Response RequestHandler::handleStatic(const Request &request)
 {
-  Response res;
+    Response res;
 
-  std::string fullPath = "www" + request.getPath();
-  if (fullPath == "www/")
-    fullPath = "www/index.html";
-  std::ifstream file(fullPath.c_str());
-  if (!file)
-  {
-    res.setStatusCode(404);
-    res.setBodyFromFile("www/error.html");
-    std::cerr << "Resourse not found!" << std::endl; // TODO: return 404
-                                                     // response
-  }
-  if (file)
-  {
-    res.setStatusCode(200);
-    res.setBodyFromFile(fullPath);
-  }
-  return res;
+    std::string fullPath = "www" + request.getPath();
+    if (fullPath == "www/")
+        fullPath = "www/index.html";
+    std::ifstream file(fullPath.c_str());
+    if (!file)
+    {
+        res.setStatusCode(404);
+        res.setBodyFromFile("www/error.html");
+        std::cerr << "Resourse not found!" << std::endl; // TODO: return 404
+                                                         // response
+    }
+    if (file)
+    {
+        res.setStatusCode(200);
+        res.setBodyFromFile(fullPath);
+    }
+    return res;
 }
 
 static Response buildCgiResponse(const std::string &cgiOutput)
@@ -165,19 +166,85 @@ static bool routeHasCgiConfig(const RouteConfig &route)
     return (!route.cgi.empty());
 }
 
-Response RequestHandler::handleRequest(const Request &request, const RouteConfig &route)
+static std::string trimHeaderValue(const std::string &value)
 {
-  // Generate a response
-  Response response;
+    size_t start;
+    size_t end;
+
+    start = 0;
+    while (start < value.length() && (value[start] == ' ' || value[start] == '\t'))
+        start++;
+    end = value.length();
+    while (end > start && (value[end - 1] == ' ' || value[end - 1] == '\t' || value[end - 1] == '\r'))
+        end--;
+    return (value.substr(start, end - start));
+}
+
+static std::string getHeaderValue(const Request &request, const std::string &name)
+{
+    std::map<std::string, std::string>::const_iterator it;
+
+    it = request.getHeaders().find(name);
+    if (it != request.getHeaders().end())
+        return (trimHeaderValue(it->second));
+    return ("");
+}
+
+static std::string getContentLength(const Request &request)
+{
+    std::string contentLength;
+
+    contentLength = getHeaderValue(request, "Content-Length");
+    if (!contentLength.empty())
+        return (contentLength);
+    if (!request.getBody().empty())
+        return (intToString(request.getBody().length()));
+    return ("");
+}
+
+static void addStandardCgiVariables(CgiContext &context, const Request &request, const ServerConfig &server)
+{
+    context.standard.values["AUTH_TYPE"] = "";
+    context.standard.values["CONTENT_LENGTH"] = getContentLength(request);
+    context.standard.values["CONTENT_TYPE"] = getHeaderValue(request, "Content-Type");
+    context.standard.values["GATEWAY_INTERFACE"] = "CGI/1.1";
+    context.standard.values["PATH_INFO"] = "";
+    context.standard.values["PATH_TRANSLATED"] = "";
+    context.standard.values["QUERY_STRING"] = getQueryString(request.getPath());
+    context.standard.values["REMOTE_ADDR"] = "";
+    context.standard.values["REMOTE_HOST"] = "";
+    context.standard.values["REMOTE_IDENT"] = "";
+    context.standard.values["REMOTE_USER"] = "";
+    context.standard.values["REQUEST_METHOD"] = request.getMethod();
+    context.standard.values["SCRIPT_NAME"] = getPathWithoutQuery(request.getPath());
+    context.standard.values["SERVER_NAME"] = server.serverName;
+    context.standard.values["SERVER_PORT"] = intToString(server.listen.port);
+    context.standard.values["SERVER_PROTOCOL"] = request.getVersion();
+    context.standard.values["SERVER_SOFTWARE"] = "webserv/1.0";
+}
+
+static CgiContext buildCgiContext(const Request &request, const RouteConfig &route, const ServerConfig &server, const std::string &executable)
+{
+    CgiContext context;
+
+    context.executable = executable;
+    context.scriptPath = buildCgiScriptPath(request, route);
+    context.requestBody = request.getBody();
+    addStandardCgiVariables(context, request, server);
+    return (context);
+}
+
+Response RequestHandler::handleRequest(const Request &request, const RouteConfig &route, const ServerConfig &server)
+{
+    // Generate a response
+    Response response;
 
     if (isCgiRequest(request.getPath(), route))
     {
-        std::string scriptPath = buildCgiScriptPath(request, route);
-        std::cout << "CGI script path: " << scriptPath << std::endl;
-        std::string queryString = getQueryString(request.getPath());
         std::string executable = getCgiExecutable(request.getPath(), route.cgi);
-
-        std::string cgiOutput = CgiHandler::runCgi(executable, scriptPath, queryString);
+        CgiContext context = buildCgiContext(request, route, server, executable);
+        std::cout << "CGI script path: " << context.scriptPath << std::endl;
+        std::string cgiOutput = CgiHandler::runCgi(context);
         return (buildCgiResponse(cgiOutput));
     }
 
@@ -192,22 +259,22 @@ Response RequestHandler::handleRequest(const Request &request, const RouteConfig
         return (response);
     }
 
-  // if (!request.getIsCgi())
-  //   CgiHandler::runCgi();
-  if (request.getMethod() == "GET")
-  {
-    response = RequestHandler::handleStatic(request);
-  }
-  else
-  {
-    response.setStatusCode(405);
-    response.setBody(
-        "<html><body><h1>405 Method Not Allowed</h1></body></html>");
-  }
-  std::ostringstream oss;
-  oss << response.getBody().length();
-  response.addHeader("Content-Type", "text/html");
-  response.addHeader("Connection", "close");
-  response.addHeader("Content-Length", oss.str());
-  return response;
+    // if (!request.getIsCgi())
+    //   CgiHandler::runCgi();
+    if (request.getMethod() == "GET")
+    {
+        response = RequestHandler::handleStatic(request);
+    }
+    else
+    {
+        response.setStatusCode(405);
+        response.setBody(
+            "<html><body><h1>405 Method Not Allowed</h1></body></html>");
+    }
+    std::ostringstream oss;
+    oss << response.getBody().length();
+    response.addHeader("Content-Type", "text/html");
+    response.addHeader("Connection", "close");
+    response.addHeader("Content-Length", oss.str());
+    return response;
 }

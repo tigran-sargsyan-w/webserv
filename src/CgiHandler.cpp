d40aabcd67c79b4edd7bf68734979499fe7f1c7f
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-12 21:48:10 +0200

    feat: enhance debugPrintEnv to include title for better context in environment logging

--- src/CgiHandler.cpp
+++ src/CgiHandler.cpp
@@ -6,7 +6,7 @@
 #include <cerrno>
 #include <cstring>
 
-static void debugPrintEnv(const CgiEnv &env);
+static void debugPrintEnv(const std::string &title, const CgiEnv &env);
 
 CgiHandler::CgiHandler() {}
 CgiHandler::~CgiHandler() {}
@@ -38,9 +38,9 @@ CgiEnv CgiHandler::buildEnvironment(const CgiContext &context)
 	envStandard = context.standard.values;
 	envHttp = context.httpHeaders.values;
 	envImplementation = context.implementation.values;
-	debugPrintEnv(context.standard.values);
-	debugPrintEnv(context.httpHeaders.values);
-	debugPrintEnv(context.implementation.values);
+	debugPrintEnv("CGI ENV - STANDARD VARS", context.standard.values);
+	debugPrintEnv("CGI ENV - HTTP HEADERS", context.httpHeaders.values);
+	debugPrintEnv("CGI ENV - IMPLEMENTATION VARS", context.implementation.values);
 	mergeEnvironment(env, envStandard);
 	mergeEnvironment(env, envHttp);
 	mergeEnvironment(env, envImplementation);
@@ -76,19 +76,18 @@ std::vector<char *> CgiHandler::buildEnvironmentPointers(std::vector<std::string
 	return (envp);
 }
 
-static void debugPrintEnv(const CgiEnv &env)
+static void debugPrintEnv(const std::string &title, const CgiEnv &env)
 {
 	CgiEnv::const_iterator it;
 
-	std::cout << "\n===== CGI ENV DEBUG =====" << std::endl;
+	std::cout << "\n===== " << title << " =====" << std::endl;
 	it = env.begin();
 	while (it != env.end())
 	{
 		std::cout << it->first << "=" << it->second << std::endl;
 		it++;
 	}
-	std::cout << "=========================\n"
-			  << std::endl;
+	std::cout << "===================================\n" << std::endl;
 }
 
 std::string CgiHandler::runCgi(const CgiContext &context)

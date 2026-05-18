7499988ee6e80f38597ef22e1bbb671c7cad57a4
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-18 21:13:28 +0200

    feat: add CgiRequestHandler class definition for handling CGI requests

---
+++ include/CgiRequestHandler.hpp
@@ -0,0 +1,23 @@
+#ifndef CGIREQUESTHANDLER_HPP
+#define CGIREQUESTHANDLER_HPP
+
+#include "Config.hpp"
+#include "Request.hpp"
+#include "Response.hpp"
+
+#include <string>
+
+class CgiRequestHandler
+{
+public:
+    static bool isCgiRequest(const Request &request, const RouteConfig &route);
+    static Response handle(const Request &request, const RouteConfig &route, const ServerConfig &server, const std::string &remoteAddr);
+
+private:
+    CgiRequestHandler();
+    CgiRequestHandler(const CgiRequestHandler &other);
+    CgiRequestHandler &operator=(const CgiRequestHandler &other);
+    ~CgiRequestHandler();
+};
+
+#endif

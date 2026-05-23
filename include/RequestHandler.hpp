1688723327dc149e59411d0871b09ef0dc28e275
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-23 18:19:14 +0200

    feat: update RequestHandler methods to include ServerConfig parameter

--- include/RequestHandler.hpp
+++ include/RequestHandler.hpp
@@ -12,9 +12,9 @@ class RequestHandler
 		~RequestHandler ();
 
 		static Response handleRequest (const Request &request, const RouteConfig &route, const ServerConfig &server, const std::string &remoteAddr);
-		static Response handleStatic (const Request &request, const RouteConfig &route);
-		static Response handlePost (const Request &request, const RouteConfig &route);
-		static Response handleHttpDelete (const Request &request, const RouteConfig &route);
+		static Response handleStatic (const Request &request, const RouteConfig &route, const ServerConfig &server);
+		static Response handlePost (const Request &request, const RouteConfig &route, const ServerConfig &server);
+		static Response handleHttpDelete (const Request &request, const RouteConfig &route, const ServerConfig &server);
 };
 
 #endif

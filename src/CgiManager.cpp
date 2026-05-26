71db18ca283288aa1b958973db458dcda65ab7f5
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-26 21:18:03 +0200

    refactor: streamline CgiManager constructor and destructor, and enhance cleanup methods

--- src/CgiManager.cpp
+++ src/CgiManager.cpp
@@ -1,17 +1,16 @@
 #include "CgiManager.hpp"
+#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-CgiManager::CgiManager()
-{
-}
+CgiManager::CgiManager() {}
 
 CgiManager::CgiManager(const CgiManager &other)
 {
 	*this = other;
 }
 
-CgiManager::~CgiManager()
-{
-}
+CgiManager::~CgiManager() {}
 
 CgiManager &CgiManager::operator=(const CgiManager &other)
 {
@@ -44,4 +43,42 @@ bool CgiManager::getClientFd(int cgiFd, int &clientFd) const
 		return (false);
 	clientFd = it->second;
 	return (true);
+}
+
+void CgiManager::closeCgiFd(int fd, PollManager &pollManager)
+{
+	if (fd == -1)
+		return;
+
+	close(fd);
+	pollManager.removeFd(fd);
+	unregisterCgiFd(fd);
+}
+
+void CgiManager::resetState(Client &client)
+{
+	client.cgiPid = -1;
+	client.cgiStdinFd = -1;
+	client.cgiStdoutFd = -1;
+	client.cgiInputBuffer.clear();
+	client.cgiInputSent = 0;
+	client.cgiOutputBuffer.clear();
+	client.cgiStdinClosed = true;
+	client.cgiStdoutClosed = true;
+	client.cgiFinished = false;
+	client.cgiStartTime = 0;
+}
+
+void CgiManager::cleanup(Client &client, PollManager &pollManager)
+{
+	if (client.cgiStdinFd != -1)
+		closeCgiFd(client.cgiStdinFd, pollManager);
+	if (client.cgiStdoutFd != -1)
+		closeCgiFd(client.cgiStdoutFd, pollManager);
+	if (client.cgiPid > 0)
+	{
+		kill(client.cgiPid, SIGKILL);
+		waitpid(client.cgiPid, NULL, 0);
+	}
+	resetState(client);
 }

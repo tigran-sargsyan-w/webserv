1430964ebfb0dfc2cbb74580d535bbc5ab636297
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-26 23:28:09 +0200

    refactor: reorganize CgiManager methods for improved readability and structure

--- include/CgiManager.hpp
+++ include/CgiManager.hpp
@@ -19,29 +19,28 @@ public:
 	CgiManager &operator=(const CgiManager &other);
 
 	bool isCgiFd(int fd) const;
-	void registerCgiFd(int cgiFd, int clientFd);
-	void unregisterCgiFd(int cgiFd);
-	bool getClientFd(int cgiFd, int &clientFd) const;
-
-    void closeCgiFd(int fd, PollManager &pollManager);
     void cleanup(Client &client, PollManager &pollManager);
-    void resetState(Client &client);
-
-    int writeToCgi(Client &client, PollManager &pollManager);
-    int readFromCgi(Client &client, PollManager &pollManager);
-    int checkFinished(Client &client);
-
     int getPollTimeoutMs(const std::map<int, Client> &clients) const;
     int checkTimeouts(std::map<int, Client> &clients, const std::vector<ServerConfig> &configs, PollManager &pollManager);
-    void finishResponse(Client &client, PollManager &pollManager);
-    void failResponse(Client &client, int code, const std::string &message, const std::vector<ServerConfig> &configs, PollManager &pollManager);
-
     int handleEvent(int cgiFd, short revents, std::map<int, Client> &clients, const std::vector<ServerConfig> &configs, PollManager &pollManager);
 
     int startForClient(Client &client, const RouteConfig &route, const ServerConfig &server, PollManager &pollManager);
 
 private:
 	std::map<int, int> cgiFdToClientFd;
+
+	void registerCgiFd(int cgiFd, int clientFd);
+	void unregisterCgiFd(int cgiFd);
+	bool getClientFd(int cgiFd, int &clientFd) const;
+
+    void closeCgiFd(int fd, PollManager &pollManager);
+    void resetState(Client &client);
+
+    int writeToCgi(Client &client, PollManager &pollManager);
+    int readFromCgi(Client &client, PollManager &pollManager);
+    int checkFinished(Client &client);
+    void finishResponse(Client &client, PollManager &pollManager);
+    void failResponse(Client &client, int code, const std::string &message, const std::vector<ServerConfig> &configs, PollManager &pollManager);
 };
 
 #endif

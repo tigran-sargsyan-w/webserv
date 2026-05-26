ef716e7fc80c1089e8c08e9d648268e244d7d88d
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-05-26 21:43:55 +0200

    refactor: implement write and read methods for CGI interaction in CgiManager

--- src/CgiManager.cpp
+++ src/CgiManager.cpp
@@ -2,6 +2,9 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <cerrno>
+#include <unistd.h>
+#include <sys/wait.h>
 
 CgiManager::CgiManager() {}
 
@@ -81,4 +84,96 @@ void CgiManager::cleanup(Client &client, PollManager &pollManager)
 		waitpid(client.cgiPid, NULL, 0);
 	}
 	resetState(client);
+}
+
+int CgiManager::writeToCgi(Client &client, PollManager &pollManager)
+{
+	size_t remaining;
+	ssize_t bytesWritten;
+
+	if (client.cgiStdinFd == -1 || client.cgiStdinClosed)
+		return (0);
+
+	remaining = client.cgiInputBuffer.size() - client.cgiInputSent;
+	if (remaining == 0)
+	{
+		closeCgiFd(client.cgiStdinFd, pollManager);
+		client.cgiStdinFd = -1;
+		client.cgiStdinClosed = true;
+		return (0);
+	}
+
+	bytesWritten = write(client.cgiStdinFd, client.cgiInputBuffer.c_str() + client.cgiInputSent, remaining);
+
+	if (bytesWritten == -1)
+	{
+		if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)
+			return (0);
+		return (1);
+	}
+
+	if (bytesWritten == 0)
+		return (0);
+
+	client.cgiInputSent += static_cast<size_t>(bytesWritten);
+
+	if (client.cgiInputSent >= client.cgiInputBuffer.size())
+	{
+		closeCgiFd(client.cgiStdinFd, pollManager);
+		client.cgiStdinFd = -1;
+		client.cgiStdinClosed = true;
+	}
+
+	return (0);
+}
+
+int CgiManager::readFromCgi(Client &client, PollManager &pollManager)
+{
+	char buffer[4096];
+	ssize_t bytesRead;
+
+	if (client.cgiStdoutFd == -1 || client.cgiStdoutClosed)
+		return (0);
+
+	bytesRead = read(client.cgiStdoutFd, buffer, sizeof(buffer));
+	if (bytesRead == -1)
+	{
+		if (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR)
+			return (0);
+		return (1);
+	}
+
+	if (bytesRead == 0)
+	{
+		closeCgiFd(client.cgiStdoutFd, pollManager);
+		client.cgiStdoutFd = -1;
+		client.cgiStdoutClosed = true;
+		return (0);
+	}
+
+	client.cgiOutputBuffer.append(buffer, bytesRead);
+	return (0);
+}
+
+int CgiManager::checkFinished(Client &client)
+{
+	int status;
+	pid_t result;
+
+	if (client.cgiPid <= 0)
+		return (0);
+
+	result = waitpid(client.cgiPid, &status, WNOHANG);
+	if (result == 0)
+		return (0);
+	if (result != client.cgiPid)
+		return (1);
+
+	client.cgiPid = -1;
+	client.cgiFinished = true;
+
+	if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+		return (0);
+
+	return (1);
 }

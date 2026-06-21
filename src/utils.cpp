8c3bb6c6e4ce9be5a6de2d5fb1b6acfe16615b97
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-06-21 21:00:32 +0200

    refactor: update intToString function declaration for consistency

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,7 +1,24 @@
 #include <sstream>
 
-std::string intToString(int num) {
+std::string intToString(int num)
+{
   std::ostringstream oss;
   oss << num;
   return (oss.str());
+}
+
+std::string toLowerCase(const std::string &value)
+{
+  std::string result;
+  size_t index;
+
+  result = value;
+  index = 0;
+  while (index < result.length())
+  {
+    result[index] = static_cast<char>(
+        std::tolower(static_cast<unsigned char>(result[index])));
+    index++;
+  }
+  return (result);
 }

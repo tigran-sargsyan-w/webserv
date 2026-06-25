6aa003874e584db156eb6e4be504e74bfac05a5b
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-06-25 20:25:44 +0200

    refactor: streamline conditional checks and variable declarations in Router class

--- src/Router.cpp
+++ src/Router.cpp
@@ -9,26 +9,24 @@ bool Router::matches(const std::string &routePath, const std::string &requestPat
 		return (true);
 	if (requestPath.find(routePath) != 0)
 		return (false);
-	if (requestPath.length() > routePath.length()
-		&& requestPath[routePath.length()] == '/')
+	if (requestPath.length() > routePath.length() && requestPath[routePath.length()] == '/')
 		return (true);
 	return (false);
 }
 
 const RouteConfig &Router::resolve(const ServerConfig &server, const std::string &requestTarget)
 {
-	const RouteConfig	*bestRoute;
-	std::string			requestPath;
-	size_t				bestLength;
+	const RouteConfig *bestRoute;
+	std::string requestPath;
+	size_t bestLength;
 
 	requestPath = UriUtils::getPathWithoutQuery(requestTarget);
 	bestRoute = NULL;
 	bestLength = 0;
 	for (std::vector<RouteConfig>::const_iterator it = server.routes.begin();
-		it != server.routes.end(); ++it)
+		 it != server.routes.end(); ++it)
 	{
-		if (matches(it->path, requestPath)
-			&& it->path.length() > bestLength)
+		if (matches(it->path, requestPath) && it->path.length() > bestLength)
 		{
 			bestRoute = &(*it);
 			bestLength = it->path.length();

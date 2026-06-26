a3bd9e240e0ac0b0aa4f582af85130f271ceacf5
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-06-26 23:10:15 +0200

    refactor: streamline dispatch and prepareResponse methods for improved readability

--- src/RequestDispatcher.cpp
+++ src/RequestDispatcher.cpp
@@ -10,45 +10,29 @@
 
 #include <iostream>
 
-RequestDispatcher::Result RequestDispatcher::dispatch(
-	Client &client,
-	const ServerConfig &server,
-	CgiManager &cgiManager,
-	PollManager &pollManager)
+RequestDispatcher::Result RequestDispatcher::dispatch(Client &client, const ServerConfig &server,
+													  CgiManager &cgiManager, PollManager &pollManager)
 {
-	const RouteConfig &route = Router::resolve(
-		server,
-		client.getRequest().getPath());
+	const RouteConfig &route = Router::resolve(server, client.getRequest().getPath());
 
 	std::cout << "Matched route: " << route.path << std::endl;
 
 	if (CgiRequestHandler::isCgiRequest(client.getRequest(), route))
 	{
-		if (cgiManager.startForClient(
-				client,
-				route,
-				server,
-				pollManager) != 0)
+		if (cgiManager.startForClient(client, route, server, pollManager) != 0)
 			return (DISPATCH_FAILED);
 		return (ASYNC_STARTED);
 	}
 
-	Response response = RequestHandler::handleRequest(
-		client.getRequest(),
-		route,
-		server);
-
+	Response response = RequestHandler::handleRequest(client.getRequest(), route, server);
 	prepareResponse(client, response);
-
 	std::cout << "Response to client:\n\n"
 			  << client.responseBuffer << std::endl;
 
 	return (RESPONSE_READY);
 }
 
-void RequestDispatcher::prepareResponse(
-	Client &client,
-	const Response &response)
+void RequestDispatcher::prepareResponse(Client &client, const Response &response)
 {
 	client.responseBuffer = response.toString();
 	client.bytesSent = 0;

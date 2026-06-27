a6d2f0fd4cf0f2e80aa3440ffc7ed3750de14e0b
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-06-27 15:17:10 +0200

    refactor: remove unused request handling methods from RequestHandler

--- include/RequestHandler.hpp
+++ include/RequestHandler.hpp
@@ -12,9 +12,6 @@ class RequestHandler
 		~RequestHandler ();
 
 		static Response handleRequest (const Request &request, const RouteConfig &route, const ServerConfig &server);
-		static Response handleStatic (const Request &request, const RouteConfig &route, const ServerConfig &server);
-		static Response handlePost (const Request &request, const RouteConfig &route, const ServerConfig &server);
-		static Response handleHttpDelete (const Request &request, const RouteConfig &route, const ServerConfig &server);
 };
 
 #endif

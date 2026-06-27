22681bac73838c876f0569d4b6199a56a57d9771
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-06-27 15:31:24 +0200

    refactor: apply OCF to request handler

--- include/RequestHandler.hpp
+++ include/RequestHandler.hpp
@@ -8,10 +8,13 @@
 class RequestHandler
 {
 	public:
-		RequestHandler ();
-		~RequestHandler ();
+		static Response handleRequest(const Request &request, const RouteConfig &route, const ServerConfig &server);
 
-		static Response handleRequest (const Request &request, const RouteConfig &route, const ServerConfig &server);
+	private:
+		RequestHandler();
+		RequestHandler(const RequestHandler &other);
+		RequestHandler &operator=(const RequestHandler &other);
+		~RequestHandler();
 };
 
 #endif

23112006e381500dc433ffc15145887bd8ff238c
tigran-sargsyan <tigran.sargsyan.w@gmail.com>
2026-06-27 15:19:41 +0200

    refactor: remove unused methods and streamline request handling in RequestHandler

--- src/RequestHandler.cpp
+++ src/RequestHandler.cpp
@@ -1,20 +1,4 @@
 #include "RequestHandler.hpp"
-#include "PathUtils.hpp"
-#include "UriUtils.hpp"
-
-#include <sys/stat.h>
-#include <sys/time.h>
-
-#include <climits>
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <sstream>
-#include <string>
-#include <unistd.h>
-#include <utils.hpp>
-
 #include "CgiRequestHandler.hpp"
 #include "Config.hpp"
 #include "ErrorResponseHandler.hpp"
@@ -23,51 +7,16 @@
 #include "Request.hpp"
 #include "Response.hpp"
 #include "StaticFileHandler.hpp"
+#include "UploadHandler.hpp"
+#include "DeleteHandler.hpp"
+
+#include <sstream>
+#include <string>
+#include <map>
 
 RequestHandler::RequestHandler () {}
 
 RequestHandler::~RequestHandler () {}
-
-static std::string getSafeUploadPath (const Request &request, const RouteConfig &route)
-{
-	if (route.uploadStore.empty ())
-		return "";
-
-	std::string decoded = UriUtils::decodePath (UriUtils::getPathWithoutQuery (request.getPath ()));
-
-	if (decoded.find ('\0') != std::string::npos)
-		return "";
-
-	if (decoded.find ("/../") != std::string::npos || decoded.find ("/..") == decoded.length () - 3)
-		return "";
-
-	std::string fileName = PathUtils::getFileName (decoded);
-
-	if (fileName.empty () || fileName == ".." || fileName == ".")
-		return "";
-
-	struct stat storeStat;
-	if (stat (route.uploadStore.c_str (), &storeStat) != 0 || !S_ISDIR (storeStat.st_mode))
-		return "";
-
-	if (access (route.uploadStore.c_str (), W_OK) != 0)
-		return "";
-
-	std::string storePath = route.uploadStore;
-	if (storePath[storePath.length () - 1] != '/')
-		storePath += '/';
-
-	return (storePath + fileName);
-}
-
-static Response buildSuccessResponse (int successCode, const std::string &successMessage)
-{
-	Response res;
-
-	res.setStatusCode (successCode);
-	res.setBody ("<html><body><h1>" + successMessage + "</h1></body></html>");
-	return res;
-}
 
 static Response validateMethod (const RouteConfig &route, HttpMethod method, const ServerConfig &server)
 {
@@ -98,103 +47,6 @@ static bool hasHeader (const Response &response, const std::string &name)
 	return (headers.find (name) != headers.end ());
 }
 
-Response RequestHandler::handleStatic (const Request &request, const RouteConfig &route, const ServerConfig &server)
-{
-	return (StaticFileHandler::handle (request, route, server));
-}
-
-Response RequestHandler::handlePost (const Request &request, const RouteConfig &route, const ServerConfig &server)
-{
-	Response response;
-
-	// 1. Check the activation of the upload
-	if (!route.uploadEnable)
-		response = ErrorResponseHandler::build (403, "Forbidden: Upload is disabled for this route", server);
-
-	// 2. Check the presence of a storage folder
-	else if (route.uploadStore.empty ())
-		response = ErrorResponseHandler::build (500, "Internal Server Error: Upload store not configured", server);
-
-	// 3. Check the body
-	else if (request.getBody ().empty ())
-		response = ErrorResponseHandler::build (400, "Bad request: Empty body", server);
-
-	else
-	{
-		// 4. Extraction and safety check of the filename
-		std::string fullPath = getSafeUploadPath (request, route);
-		if (fullPath.empty ())
-			response = ErrorResponseHandler::build (400, "Bad Request: Invalid file name", server);
-		else
-		{
-			// 5. Check if a directory with this name already exists
-			struct stat pathStat;
-			if (stat (fullPath.c_str (), &pathStat) == 0 && S_ISDIR (pathStat.st_mode))
-				response = ErrorResponseHandler::build (409, "Conflict: A directory with this name already exists", server);
-			else
-			{
-				// 6. Try to open the file for writing
-				std::ofstream file (fullPath.c_str (), std::ios::out | std::ios::binary);
-				if (!file.is_open ())
-					response = ErrorResponseHandler::build (500, "Internal Server Error: Could not open file", server);
-				else
-				{
-					// 7. Write the body data
-					file.write (request.getBody ().data (), request.getBody ().size ());
-					file.close ();
-
-					// 8. Check if disk write failed (disk full)
-					if (file.fail ())
-					{
-						std::remove (fullPath.c_str ());
-						response = ErrorResponseHandler::build (500, "Internal Server Error: Write failed", server);
-					}
-					// 9. Success response
-					else
-						response = buildSuccessResponse (201, "Created: File uploaded");
-				}
-			}
-		}
-	}
-	return response;
-}
-
-Response RequestHandler::handleHttpDelete (const Request &request, const RouteConfig &route, const ServerConfig &server)
-{
-	Response response;
-
-	// 1. Check the activation of the upload
-	if (!route.uploadEnable)
-		response = ErrorResponseHandler::build (403, "Forbidden: Delete is disabled for this route", server);
-	else
-	{
-		// 2. Check that the path is safe and create the complete path
-		std::string fullPath = getSafeUploadPath (request, route);
-		if (fullPath.empty ())
-			response = ErrorResponseHandler::build (403, "Forbidden: Invalid path sequence", server);
-		else
-		{
-			// 3. Check if the resource exists
-			struct stat pathStat;
-			if (stat (fullPath.c_str (), &pathStat) != 0)
-				response = ErrorResponseHandler::build (404, "Not Found: Resource does not exist", server);
-
-			// 4. Forbid the deletion of folders
-			else if (S_ISDIR (pathStat.st_mode))
-				response = ErrorResponseHandler::build (403, "Forbidden: Cannot delete a directory", server);
-			else
-			{
-				// 5. Try to delete
-				if (std::remove (fullPath.c_str ()) == 0)
-					response = buildSuccessResponse (200, "File deleted successfully");
-				else
-					response = ErrorResponseHandler::build (500, "Internal Server Error: Failed to delete the file", server);
-			}
-		}
-	}
-	return response;
-}
-
 Response RequestHandler::handleRequest (const Request &request, const RouteConfig &route, const ServerConfig &server)
 {
 	Response response;
@@ -223,16 +75,16 @@ Response RequestHandler::handleRequest (const Request &request, const RouteConfi
 		switch (method)
 		{
 			case HTTP_GET:
-				response = RequestHandler::handleStatic (request, route, server);
+				response = StaticFileHandler::handle(request, route, server);
 				break;
 			case HTTP_POST:
-				response = RequestHandler::handlePost (request, route, server);
+				response = UploadHandler::handle(request, route, server);
 				break;
 			case HTTP_DELETE:
-				response = RequestHandler::handleHttpDelete (request, route, server);
+				response = DeleteHandler::handle(request, route, server);
 				break;
 			default:
-				response = ErrorResponseHandler::build (500, "Internal Server Error", server);
+				response = ErrorResponseHandler::build(500, "Internal Server Error", server);
 		}
 	}
 

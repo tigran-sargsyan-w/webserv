aeae935016448d51daec3c1affb3489fdfa2f739
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-06-29 20:51:56 +0200

    refactor: move response application to helper

---
+++ src/ClientResponseApplier.cpp
@@ -0,0 +1,9 @@
+#include "ClientResponseApplier.hpp"
+
+void	ClientResponseApplier::apply(Client &client, const Response &response)
+{
+	client.responseBuffer = response.toString();
+	client.bytesSent = 0;
+	client.responseReady = true;
+	client.state = WRITING;
+}

b18020eb8ece6ea142f4f55088d155381846eb19
Camille <camille.fabre003@gmail.com>
2026-07-02 18:04:05 +0200

    feat: add header for client timeout

---
+++ include/ClientTimeoutHandler.hpp
@@ -0,0 +1,22 @@
+#ifndef CLIENT_TIMEOUT_HANDLER_HPP
+# define CLIENT_TIMEOUT_HANDLER_HPP
+
+# include "Client.hpp"
+
+# include <ctime>
+# include <map>
+# include <vector>
+
+static const int CLIENT_TIMEOUT_SECONDS = 30;
+
+class ClientTimeoutHandler
+{
+	public:
+		
+
+	private:
+		ClientTimeoutHandler();
+		
+};
+
+#endif

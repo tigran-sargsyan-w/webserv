bcf56fe3e4f3b4b5c2a9a62e2a71afd40e4bc301
Camille <camille.fabre003@gmail.com>
2026-07-02 18:08:34 +0200

    feat: declare client timeout handler methods based on cgi timeout handler

--- include/ClientTimeoutHandler.hpp
+++ include/ClientTimeoutHandler.hpp
@@ -12,11 +12,15 @@ static const int CLIENT_TIMEOUT_SECONDS = 30;
 class ClientTimeoutHandler
 {
 	public:
-		
+		static int	getPollTimeoutMs(const std::map<int, Client> &clients,
+					int timeoutSeconds);
+		static void	collectExpiredClients(const std::map<int, Client> &clients,
+					int timeoutSeconds, std::vector<int> &expiredFds);
 
 	private:
 		ClientTimeoutHandler();
-		
+		static bool	isExpired(const Client &client, time_t now,
+					int timeoutSeconds);
 };
 
 #endif

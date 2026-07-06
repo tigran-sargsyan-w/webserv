c2f9884c83c6aed3e3115e463a0e52068ad68830
Tigran Sargsyan <tigran.sargsyan.w@gmail.com>
2026-07-06 21:46:52 +0200

    Use TemplateRenderer in UploadStorage

--- src/UploadStorage.cpp
+++ src/UploadStorage.cpp
@@ -1,109 +1,23 @@
 #include "UploadStorage.hpp"
 #include "ErrorResponseHandler.hpp"
 #include "PathUtils.hpp"
+#include "TemplateRenderer.hpp"
 
 #include <cstdio>
-#include <cctype>
 #include <fstream>
-#include <iomanip>
-#include <sstream>
 #include <string>
 #include <sys/stat.h>
 
 namespace
 {
-	std::string	htmlEscape(const std::string &text)
-	{
-		std::string	result;
-		size_t		index;
-
-		index = 0;
-		while (index < text.length())
-		{
-			if (text[index] == '&')
-				result += "&amp;";
-			else if (text[index] == '<')
-				result += "&lt;";
-			else if (text[index] == '>')
-				result += "&gt;";
-			else if (text[index] == '"')
-				result += "&quot;";
-			else if (text[index] == '\'')
-				result += "&#39;";
-			else
-				result += text[index];
-			index++;
-		}
-		return (result);
-	}
-
-	bool	isUrlSafeChar(unsigned char c)
-	{
-		if (std::isalnum(c))
-			return (true);
-		if (c == '-' || c == '_' || c == '.' || c == '~')
-			return (true);
-		return (false);
-	}
-
-	std::string	urlEncodePathSegment(const std::string &text)
-	{
-		std::ostringstream	stream;
-		size_t				index;
-		unsigned char		c;
-
-		index = 0;
-		while (index < text.length())
-		{
-			c = static_cast<unsigned char>(text[index]);
-			if (isUrlSafeChar(c))
-				stream << text[index];
-			else
-			{
-				stream << '%';
-				stream << std::uppercase << std::hex << std::setw(2);
-				stream << std::setfill('0') << static_cast<int>(c);
-				stream << std::nouppercase << std::dec;
-			}
-			index++;
-		}
-		return (stream.str());
-	}
-
-	std::string	readTemplateFile(const std::string &path)
-	{
-		std::ifstream		file(path.c_str());
-		std::ostringstream	buffer;
-
-		if (!file.is_open())
-			return ("");
-		buffer << file.rdbuf();
-		return (buffer.str());
-	}
-
-	void	replaceAll(std::string &text, const std::string &from,
-		const std::string &to)
-	{
-		size_t	position;
-
-		if (from.empty())
-			return;
-		position = 0;
-		while ((position = text.find(from, position)) != std::string::npos)
-		{
-			text.replace(position, from.length(), to);
-			position += to.length();
-		}
-	}
-
 	Response	buildFallbackCreatedResponse(const std::string &fileName)
 	{
 		Response	response;
 		std::string	body;
 
 		body = "<html><body><h1>Created: File uploaded</h1>";
-		body += "<p>Stored as: " + htmlEscape(fileName) + "</p>";
-		body += "</body></html>";
+		body += "<p>Stored as: " + TemplateRenderer::htmlEscape(fileName);
+		body += "</p></body></html>";
 		response.setStatusCode(201);
 		response.setBody(body);
 		response.addHeader("Content-Type", "text/html");
@@ -114,17 +28,18 @@ namespace
 		const ServerConfig &server)
 	{
 		Response	response;
+		TemplateRenderer::Variables	variables;
 		std::string	body;
 		std::string	fileHref;
 		std::string	templatePath;
 
 		templatePath = PathUtils::join(server.root, "upload-success.html");
-		body = readTemplateFile(templatePath);
+		fileHref = "/uploads/" + TemplateRenderer::urlEncodePathSegment(fileName);
+		variables["{{FILENAME}}"] = TemplateRenderer::htmlEscape(fileName);
+		variables["{{FILE_HREF}}"] = TemplateRenderer::htmlEscape(fileHref);
+		body = TemplateRenderer::render(templatePath, variables);
 		if (body.empty())
 			return (buildFallbackCreatedResponse(fileName));
-		fileHref = "/uploads/" + urlEncodePathSegment(fileName);
-		replaceAll(body, "{{FILENAME}}", htmlEscape(fileName));
-		replaceAll(body, "{{FILE_HREF}}", htmlEscape(fileHref));
 		response.setStatusCode(201);
 		response.setBody(body);
 		response.addHeader("Content-Type", "text/html");
